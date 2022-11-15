91f0ec1b890f0dcb11cf72cd3c673de3b533fe4e
bradfordd <dbradford1337@gmail.com>
2022-11-15 18:54:50 -0500

    Access token API finished

--- test/registerAPIClient.js
+++ test/registerAPIClient.js
@@ -5,62 +5,29 @@ import { assert } from 'chai';  // Using Assert style
 import { should } from 'chai';  // Using Should style
 should();  // Modifies `Object.prototype`
 
-it('GET /status', () =>{
-        // Make a GET request to get status of bookstore
-        return request
-        .get('/status')
-        .expect(200)
-        .then((res) => {
-         // assert status returned to equal "OK"
-         expect(res.body.status).to.eq('OK');
-       });
-      });
+function makename(length) {
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
 
-it('GET /books', () =>{
-  // Get request to return all books in bookstore
+it('POST /api-clients', () =>{
+  // Make a GET request to get status of bookstore
+  const randomName = makename(6);
+  const randomEmail = randomName + "@example.com";
+  const task = {
+    clientName: randomName,
+    clientEmail: randomEmail
+  };
   return request
-  .get('/books')
-  .expect(200)
+  .post('/api-clients')
+  .send(task)
+  .expect(201)
   .then((res) => {
-    // assert status returned to equal "OK"
-    expect(res.body).to.not.be.null;
-    res.body.forEach(element => { element.should.have.property('id'); element.should.have.property('name'); element.should.have.property('type'); element.should.have.property('available');});
-    //res.body[0].should.have.property('id');
-  });
-});
-
-it ('GET /books?type=fiction', () => {
-  // Get request to return all books of type fiction
-  return request
-  .get('/books?type=fiction')
-  .expect(200)
-  .then((res) => {
-    expect(res.body).to.not.be.null;
-    res.body.forEach(element => { 
-      element.should.have.property('id'); 
-      element.should.have.property('name'); 
-      element.should.have.property('type'); 
-      element.should.have.property('available');
-    });
-    res.body.forEach(element => { 
-      element.type.should.be.eq("fiction"); 
-    });
-  });
-});
-
-it ('GET /books/:bookId', () => {
-  // Get request to return book of specified id. In this case id === 1
-  const bookid = 1
-  return request
-  .get('/books/' + bookid)
-  .expect(200)
-  .then((res) => {
-    expect(res.body).to.not.be.null;
-    expect(res.body).to.be.an('object');
-    res.body.should.have.property('id'); 
-    res.body.should.have.property('name'); 
-    res.body.should.have.property('type'); 
-    res.body.should.have.property('available');
-    res.body.id.should.be.eq(1); 
-  });
+    res.body.should.have.property('accessToken'); 
+ });
 });
